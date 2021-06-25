0ea6c2cb0cebf9dea8b04ee9e93a42473ecbc4f5
MrRobot420 <max.karlchen@web.de>
2021-06-25 22:36:28 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Max Karl on 25.06.21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
