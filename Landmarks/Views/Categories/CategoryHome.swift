4305ea2fb0f2d238dc88cd134569c2d621e30a4a
MrRobot420 <max.karlchen@web.de>
2021-06-29 20:55:06 +0200

    Completed Section 1 - Add a Category View Found here: https://developer.apple.com/tutorials/swiftui/composing-complex-interfaces

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Max Karl on 29.06.21.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
