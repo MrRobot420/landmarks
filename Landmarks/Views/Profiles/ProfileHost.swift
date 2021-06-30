9610ffb23bf2fe10078eb042e6d08e7c3a07acec
MrRobot420 <max.karlchen@web.de>
2021-06-30 18:47:35 +0200

    Completed Section 1 - Display a User Profile Found here: https://developer.apple.com/tutorials/swiftui/working-with-ui-controls

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Max Karl on 30.06.21.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
